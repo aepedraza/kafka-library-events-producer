d6a7c057c4a6acda183acda577737123cb945ed0
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2022-05-18 22:27:20 -0300

    08-28: Added LibraryEventProducer
    Send Kafka message with producer using KafkaTemplate and sending to default topic

---
+++ src/main/java/com/learnkafka/producer/LibraryEventProducer.java
@@ -0,0 +1,54 @@
+package com.learnkafka.producer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.learnkafka.domain.LibraryEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Component
+@Slf4j
+public class LibraryEventProducer {
+
+    @Autowired
+    private KafkaTemplate<Integer, String> kafkaTemplate;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
+
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = mapper.writeValueAsString(libraryEvent);
+
+        // Sends to default topic
+        ListenableFuture<SendResult<Integer, String>> resultFuture = kafkaTemplate.sendDefault(key, value);
+
+        resultFuture.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                handleFailure(key, value, ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<Integer, String> result) {
+                handleSuccess(key, value, result);
+            }
+        });
+    }
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> result) {
+        log.info("Message sent successfully. Key: {}, value: {}. Partition: {}",
+                key, value, result.getRecordMetadata().partition());
+    }
+
+    private void handleFailure(Integer key, String value, Throwable ex) {
+        log.error("Error sending message. Key: {}, value: {}", key, value);
+        ex.printStackTrace();
+    }
+}
