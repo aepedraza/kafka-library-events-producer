3451dc70ef0618ef78c08a3e91317eb61e816415
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2022-05-18 22:43:58 -0300

    08-30: Added logs to explain callback execution

--- src/main/java/com/learnkafka/controller/LibraryEventController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventController.java
@@ -3,6 +3,7 @@ package com.learnkafka.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
 import com.learnkafka.producer.LibraryEventProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/library-event")
+@Slf4j
 public class LibraryEventController {
 
     @Autowired
@@ -21,8 +23,11 @@ public class LibraryEventController {
     @PostMapping
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
 
+        log.info("Before sendLibraryEvent");
         libraryEventProducer.sendLibraryEvent(libraryEvent);
+        log.info("After sendLibraryEvent"); // logged before the logs in resultFuture callback
 
+        // Returns 201 even when sending fails. It's up to the app to recover from that error
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
 }
