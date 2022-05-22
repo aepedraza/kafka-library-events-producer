d8559bfbcde4cd55fb42865e89832dfabe46e178
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2022-05-22 18:39:34 -0300

    08-31: Added synchronous Kafka send method

--- src/main/java/com/learnkafka/producer/LibraryEventProducer.java
+++ src/main/java/com/learnkafka/producer/LibraryEventProducer.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 @Component
 @Slf4j
 public class LibraryEventProducer {
@@ -21,6 +25,12 @@ public class LibraryEventProducer {
     @Autowired
     private ObjectMapper mapper;
 
+    /**
+     * Sends event asynchronously using Kafka with default topic.
+     *
+     * @param libraryEvent event to be sent by Kafka
+     * @throws JsonProcessingException if event cannot be converted to JSON
+     */
     public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
 
         Integer key = libraryEvent.getLibraryEventId();
@@ -30,6 +40,7 @@ public class LibraryEventProducer {
         // If key is null, send to available partition using round-robin
         ListenableFuture<SendResult<Integer, String>> resultFuture = kafkaTemplate.sendDefault(key, value);
 
+        // Callback executed in a different thread
         resultFuture.addCallback(new ListenableFutureCallback<>() {
             @Override
             public void onFailure(Throwable ex) {
@@ -50,6 +61,39 @@ public class LibraryEventProducer {
 
     private void handleFailure(Integer key, String value, Throwable ex) {
         log.error("Error sending message. Key: {}, value: {}", key, value);
-        ex.printStackTrace();
+        try {
+            throw ex;
+        } catch (Throwable e) {
+            log.error("Error in onFailure: {}", e.getMessage());
+        }
+    }
+
+    /**
+     * Sends Kafka event synchronously using Kafka and the default topic
+     *
+     * @param libraryEvent event to be sent by Kafka
+     * @return send result
+     * @throws JsonProcessingException if event cannot be converted to JSON
+     * @throws ExecutionException if there is an error getting the result
+     * @throws InterruptedException if there is an error getting the result
+     * @throws TimeoutException if there is an error getting the result
+     */
+    public SendResult<Integer, String> sendLibraryEventSynchronous(LibraryEvent libraryEvent)
+            throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+
+        Integer key = libraryEvent.getLibraryEventId();
+        String value = mapper.writeValueAsString(libraryEvent);
+
+        SendResult<Integer, String> sendResult;
+        try {
+            // .get() waits until future is complete (success or failure)
+            // callback is executed in the same thread
+            sendResult = kafkaTemplate.sendDefault(key, value).get(1L, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            log.error("Error sending message. Key: {}, value: {}", key, value);
+            throw e;
+        }
+
+        return sendResult;
     }
 }
