ba10248d071667e1517fb4377df0a98c38156ea2
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2022-05-22 20:59:46 -0300

    08-34: Added type to LibraryEvent
    LibraryEventType will be used to distinguish between POST and PUT request

--- src/main/java/com/learnkafka/controller/LibraryEventController.java
+++ src/main/java/com/learnkafka/controller/LibraryEventController.java
@@ -2,6 +2,7 @@ package com.learnkafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.domain.LibraryEvent;
+import com.learnkafka.domain.LibraryEventType;
 import com.learnkafka.producer.LibraryEventProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +24,10 @@ public class LibraryEventController {
     @PostMapping
     public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
 
+        libraryEvent.setLibraryEventType(LibraryEventType.NEW);
+
         log.info("Before sendLibraryEvent");
-        libraryEventProducer.sendLibraryEvent(libraryEvent);
+        libraryEventProducer.sendLibraryEventUsingProducerRecord(libraryEvent);
         log.info("After sendLibraryEvent"); // logged before the logs in resultFuture callback
 
         // Returns 201 even when sending fails. It's up to the app to recover from that error
