f89855fd853c59311d0de197e75f316ea45746e9
Alvaro Esteban Pedraza <aepedraza3117@gmail.com>
2022-06-25 01:47:30 -0300

    11-Assignment 2: Added unit test for PUT /v1/library-event

--- src/test/java/unit/com/learnkafka/controller/LibraryEventControllerUnitTest.java
+++ src/test/java/unit/com/learnkafka/controller/LibraryEventControllerUnitTest.java
@@ -2,7 +2,6 @@ package com.learnkafka.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.learnkafka.domain.Book;
 import com.learnkafka.domain.LibraryEvent;
 import com.learnkafka.producer.LibraryEventProducer;
 import org.junit.jupiter.api.Test;
@@ -16,10 +15,11 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import static com.learnkafka.test.repository.BookForTestRepository.customBook;
 import static com.learnkafka.test.repository.BookForTestRepository.kafkaSpringBootDilip;
+import static com.learnkafka.test.repository.LibraryEventForTestRepository.libraryEventWithId;
 import static com.learnkafka.test.repository.LibraryEventForTestRepository.libraryEventWithNullId;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,6 +29,7 @@ class LibraryEventControllerUnitTest {
 
     private static final String EXPECTED_MESSAGE_BOOK_NULL = "book - must not be null";
     private static final String EXPECTED_MESSAGE_BOOK_WITH_NULL_FIELDS = "book.bookAuthor - must not be blank, book.bookId - must not be null";
+    private static final String EXPECTED_MESSAGE_LIBRARY_EVENT_ID_NULL = "libraryEventId cannot be null";
 
     @Autowired
     private MockMvc mockMvc; // access to all endpoints part of controller
@@ -82,5 +83,37 @@ class LibraryEventControllerUnitTest {
         whenProducerSendsLibraryEventReturnNull(libraryEvent);
 
         performPostAndExpect4xxWithErrorMessage(json, EXPECTED_MESSAGE_BOOK_WITH_NULL_FIELDS);
+    }
+
+    @Test
+    public void updateLibraryEvent() throws Exception {
+        LibraryEvent libraryEvent = libraryEventWithId(kafkaSpringBootDilip());
+        String json = mapper.writeValueAsString(libraryEvent);
+
+        whenProducerSendsLibraryEventReturnNull(libraryEvent);
+
+        putLibraryEventWithJsonContent(json).andExpect(status().isOk());
+    }
+
+    private ResultActions putLibraryEventWithJsonContent(String json) throws Exception {
+        return mockMvc.perform(put("/v1/library-event")
+                .content(json)
+                .contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void updateLibraryEvent_withNullLibraryEventId() throws Exception {
+        LibraryEvent libraryEvent = libraryEventWithNullId(kafkaSpringBootDilip());
+        String json = mapper.writeValueAsString(libraryEvent);
+
+        whenProducerSendsLibraryEventReturnNull(libraryEvent);
+
+        performPutAndExpectBadRequestWithErrorMessage(json, EXPECTED_MESSAGE_LIBRARY_EVENT_ID_NULL);
+    }
+
+    private void performPutAndExpectBadRequestWithErrorMessage(String json, String expectedMessage) throws Exception {
+        putLibraryEventWithJsonContent(json)
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(expectedMessage));
     }
 }
